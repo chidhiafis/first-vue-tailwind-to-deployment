617225c222fbb34ad40b9a112c012264240df625
chidhiafis <dhichiafis56@gmail.com>
2024-04-02 00:03:46 -0700

    second commit this is ti

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@ provide('dataset',dataset)
         <div class="container mx-auto">
           <div class="grid grid-cols-1 md:grid-cols-2">
             <div class="rounded-xl shadow-sm p-4">
-              <img :src="monkey" class="object-cover mx-auto">
+              <img :src="monkey" class="object-fill mx-auto opacity-35">
             </div>
             <div class="rounded-xl shadow-sm text-justify bg-white p-4">
               <h1 class="font-bold p-3 mb-2 text-[34px]">Hello i am ochieng odhiambo</h1>
@@ -29,7 +29,7 @@ provide('dataset',dataset)
         <div class="mx-auto">
           <div class="flex flex-col">
             <h3 class="p-4 mb-4 text-center text-[34px] font-bold">My project</h3>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               <div class="bg-white rounded-2xl p-4 shadow-md mb-3 mx-4" v-for="p in dataset" :key="p.id">
                 <h2 class="mb-2 font-bold text-[34px] p-3">{{ p.title }}</h2>
                 <p class="p-3 text-[22px]"> {{ p.description }}</p>
